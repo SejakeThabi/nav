cfed66f209ed4cf424cbf6a23e59d4c5c852a1ea
SejakeThabi <57217132+SejakeThabi@users.noreply.github.com>
2021-09-14 20:25:03 +0200

    Add files via upload

---
+++ src/main/java/com/example/navigationapp/AboutUs.java
@@ -0,0 +1,22 @@
+package com.example.navigationapp;
+
+import android.icu.util.RangeValueIterator;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import java.util.Calendar;
+
+
+public class AboutUs extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_us);
+    }
+
+
+    }
+
